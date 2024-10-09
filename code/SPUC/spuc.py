32e3ca7722b269ce80a74b7c7bc1208e15b20092
fherreazcue <francisco.herreazcue@gmail.com>
2024-10-09 11:56:51 +0100

    Improves units

--- code/SPUC/spuc.py
+++ code/SPUC/spuc.py
@@ -8,46 +8,50 @@ from fastapi import FastAPI
 units = None
 app = FastAPI()
 
+
 @app.put("/unicorn_spotted/")
-def unicorn_sighting(location: str, weight: float):
+def unicorn_sighting(location: str, brightness: float):
 
     # --------------------------------------------------------------------------
     # Write the sighting to a file and print to the console
-    
     with open("unicorn_sightings.txt", "a") as unicorn_file:
         # Append the location to the file
-        line = f"{datetime.now()}, {location}, {weight} {units}\n"
+        line = f"{datetime.now()}, {location}, {brightness} {units}\n"
         unicorn_file.write(line)
 
         # Print the line to the console
-        console_line = f":::: ({datetime.now()}) Unicorn spotted at {location}!! Weight: {weight} {units} ::::"
+        console_line = f":::: ({datetime.now()}) Unicorn spotted at {location}!! Brightness: {brightness} {units} ::::"
         print(console_line)
-    
+
     return {"message": "Unicorn sighting recorded!"}
 
 
 if __name__ == "__main__":
-
     # --------------------------------------------------------------------------
     # Parse the command line arguments
-    
     parser = argparse.ArgumentParser(description="Run the unicorn sighting API")
-    parser.add_argument("--units",
-                        type=str,
-                        default="imperial",
-                        choices=["imperial", "metric"],
-                        help="The units to use for the weight")
+    parser.add_argument(
+        "--units",
+        type=str,
+        default="iuhc",
+        choices=["iuhc", "iulu"],
+        help="The units to use for the unicorn brightness",
+    )
     args = parser.parse_args()
 
     # --------------------------------------------------------------------------
     # Set the units
-    
-    if args.units == "imperial":
-        units = "space-lbs"
-    else:
-        units = "kg"
+    units = args.units
+    if units == "iuhc":
+        unit_long_name = "Imperial Unicorn Hoove Candles"
+    elif units == "iulu":
+        unit_long_name = "Intergalactic Unicorn Luminiocity Units"
+
+    # --------------------------------------------------------------------------
+    # Print the initialization message
+    print(f"Initializing SPUC...")
+    print(f"Units set to {unit_long_name} [{units}].")
 
     # --------------------------------------------------------------------------
     # Run the API
-    
     uvicorn.run(app, host="0.0.0.0", port=80)
