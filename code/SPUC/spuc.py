ebd323a8df62c01ca0231b19011252209275de21
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2024-10-09 11:26:31 +0100

    Add endpoint for recording a unicorn

--- code/SPUC/spuc.py
+++ code/SPUC/spuc.py
@@ -1,15 +1,53 @@
 from typing import Union
+from datetime import datetime
+import argparse
 
+import uvicorn
 from fastapi import FastAPI
 
+units = None
 app = FastAPI()
 
+@app.put("/unicorn_spotted/")
+def unicorn_sighting(location: str, weight: float):
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    # --------------------------------------------------------------------------
+    # Write the sighting to a file and print to the console
+    
+    with open("unicorn_sightings.txt", "a") as unicorn_file:
+        # Append the location to the file
+        line = f"{datetime.now()}, {location}, {weight} {units}\n"
+        unicorn_file.write(line)
+
+        # Print the line to the console
+        console_line = f":::: ({datetime.now()}) Unicorn spotted at {location}!! Weight: {weight} {units} ::::"
+        print(console_line)
+    
+    return {"message": "Unicorn sighting recorded!"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+
+    # --------------------------------------------------------------------------
+    # Parse the command line arguments
+    
+    parser = argparse.ArgumentParser(description="Run the unicorn sighting API")
+    parser.add_argument("--units",
+                        type=str,
+                        default="imperial",
+                        choices=["imperial", "metric"],
+                        help="The units to use for the weight")
+    args = parser.parse_args()
+
+    # --------------------------------------------------------------------------
+    # Set the units
+    
+    if args.units == "imperial":
+        units = "space-lbs"
+    else:
+        units = "kg"
+
+    # --------------------------------------------------------------------------
+    # Run the API
+    
+    uvicorn.run(app, host="0.0.0.0", port=80)
